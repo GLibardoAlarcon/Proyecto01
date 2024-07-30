f9fe987b31bbfa9cd16be7038a05154c83d9241f
GLibardoAlarcon <libardoalarcon96@gmail.com>
2024-07-30 09:23:41 -0500

    Se termina el proyecto pueva 1

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 import pandas as pd
 from babel.dates import format_date
-from io import BytesIO
+from io import BytesIO 
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import hstack
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
 app = FastAPI()
+
+# Para cada función se tiene que ingresar el archivo, en la ruta de cada función contamos con un nombre que hace referencia 
+# a el archivo que consume cada una
 
 # Variable global para almacenar el DataFrame
 df_movies = None
@@ -22,7 +30,7 @@ async def upload_parquet(file: UploadFile = File(...)):
     return {"message": "Archivo Parquet cargado con éxito"}
 
 
-@app.get("/FilmacionesM/", tags=['Proyecto_01'])
+@app.get("/Movies/", tags=['Proyecto_01'])
 # Función para la cantidas de filmaciones segun el mes
 async def cantidad_filmaciones_mes(mes: str):
     # Variable que se hace cargo de llevar el conteo
@@ -42,7 +50,7 @@ async def cantidad_filmaciones_mes(mes: str):
         raise HTTPException(status_code=404, detail="No se ha cargado ningún archivo Parquet")
     return Variable
 
-@app.get("/FilmacionesD", tags=['Proyecto_01'])
+@app.get("/Movies1", tags=['Proyecto_01'])
 # Creamos la función
 async def cantidad_filmaciones_dia(dia: str):
     # Variable conteo
@@ -60,7 +68,7 @@ async def cantidad_filmaciones_dia(dia: str):
         raise HTTPException(status_code=404, detail="No se ha cargado ningún archivo Parquet")
     return{str(conteoD): "cantidad de peliculas fuerón estrenadas en los", "dias": dia}
 
-@app.get("/score", tags=['Proyecto_01'])
+@app.get("/Movies2", tags=['Proyecto_01'])
 # Función para el score por filmación
 async def score_titulo(Titulo: str):
     # Condición para saber si cargo el archivo
@@ -77,7 +85,7 @@ async def score_titulo(Titulo: str):
       raise HTTPException(status_code=404, detail="No se ha cargado ningún archivo Parquet")
     return Variable
 
-@app.get("/Votos", tags=['Proyecto_01'])
+@app.get("/Movies3", tags=['Proyecto_01'])
 # Creamos la función
 async def votos_titulo(Titulo: str):
     # Si no encuentra el titulo solicitado
@@ -96,16 +104,24 @@ async def votos_titulo(Titulo: str):
       raise HTTPException(status_code=404, detail="No se ha cargado ningún archivo Parquet")
     return Variable
 
-@app.get("/Actor", tags=['Proyecto_01'])
+@app.get("/Union", tags=['Proyecto_01'])
+# Creamos la función
 async def actor(Actor: str):
+    # Valor que toma la variable si no encuentra el dato
     Variable = {"El dato ingresado es incorrecto o no": "se encuentra en nuestra base de datos"}
+    # Variables iniciadas en 0
     conteo = 0
     retorno = 0
     promedio = 0
+    # Condición para que no puedan ingresar nulos
     if df_movies is not None:
+        # Recorrido fila a fila
         for indice, fila in df_movies.iterrows():
+           # Por medio de este ciclo puede recorrer a los 10 actores 
            for i in range(0, 9):
+               # Condición si llega a encontrar al actor digitado
                if fila[i] == Actor:
+                   # Se realiza las operaciones
                    conteo += 1
                    retorno += fila[20]
         promedio = retorno / conteo
@@ -114,19 +130,65 @@ async def actor(Actor: str):
         raise HTTPException(status_code=404, detail="No se ha cargado ningún archivo Parquet")
     return Variable
 
-@app.get("/Director", tags=['Proyecto_01'])
+@app.get("/Union1", tags=['Proyecto_01'])
 async def Director(Director: str):
+    # Se inicializa una lista y una variable en 0
     Datos = []
     Retorno = 0
+    # Condición para nulos
     if df_movies is not None:
+        #Ciclo para recorrer la columna
         for i in range(0, len(df_movies)):
+            # Condición para encontrar al director digitado
             if df_movies['crew_Director'][i] == Director:
+                # Se guardan los datos de salida en la lista
                 Datos.append({"Titulo": str(df_movies['title'][i]), "Fecha de lanzamiento": str(df_movies['release_date'][i]), "Costo": str(df_movies['budget'][i]), "Ganancia": str(df_movies['revenue'][i] - df_movies['budget'][i])})
                 Retorno += df_movies['return'][i]
         Datos.append({"Exito segun retorno": str(Retorno)})
     else:
         raise HTTPException(status_code=404, detail="No se ha cargado ningún archivo Parquet")
     return Datos        
+
+@app.get("/Movies4", tags=['Proyecto_01'])
+async def Recomendaciones(Pelicula: str):
+    # Convertimos los titulos de las peliculas en una matriz TF-IDF
+    vectorizer = TfidfVectorizer(stop_words='english')
+    X_text = vectorizer.fit_transform(df_movies['title'])
+    #Normalizacion de variables númericas
+    scaler = StandardScaler()
+    X_numeric = scaler.fit_transform(df_movies[['popularity', 'vote_count']])
+    # Concatenamos las caracteristicas de texto y númericas
+    X = hstack([X_text, X_numeric])
+
+    # Definimos la cantidad de clusters
+    num_clusters = 3
+    #Aplicamos K-means
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    kmeans.fit(X)
+
+    # Cremos una nueva columna para poder poner las etiquetas de los clusters
+    df_movies['Clusters'] = kmeans.labels_
+
+    # Obtenemos el indice de la pelicula solicitada
+    idx = df_movies[df_movies['title'] == Pelicula].index[0]
+
+    # Obtenemos el cluster de la pelicula solicitada
+    movie_cluster = df_movies.loc[idx, 'Clusters']
+
+    # Filtrar las peliculas que estan en el mismo cluster
+    cluster_indices = df_movies[df_movies['Clusters'] == movie_cluster].index
+
+    # Calculamos las similitud de coseno entre la pelicula solicitada y las peliculas que estan en el mismi cluster
+    coseno_similares = cosine_similarity(X_text[idx], X_text[cluster_indices]).flatten()
+
+    # Ordenar las peliculas por similitud
+    similar_indices = coseno_similares.argsort()[-5-1:-1][::-1]
+
+    # Oredenar los titulos de las peliculas recomendadas
+    recommended_movies = df_movies.iloc[cluster_indices[similar_indices]]['title']
+
+
+    return recommended_movies
     
 
 
